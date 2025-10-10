c0ea81fdfbdae8bb076ab9493f75d6c06fe177fa
Yuom Kitya <kityayuom@gmail.com>
2025-10-10 13:53:19 +0700

    add color

--- src/atm/Main.java
+++ src/atm/Main.java
@@ -1,17 +1,32 @@
 package atm;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String CYAN = "\u001B[36m";
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+
+            System.out.println(BLUE + "╔══════════════════════════════╗" + RESET);
+            System.out.println(BLUE + "║        ATM SIMULATION        ║" + RESET);
+            System.out.println(BLUE + "╠══════════════════════════════╣" + RESET);
+
+            System.out.println(GREEN + "║  [1] User Login              ║" + RESET);
+            System.out.println(YELLOW + "║  [2] Admin Login             ║" + RESET);
+            System.out.println(RED + "║  [0] Exit                    ║" + RESET);
+
+            System.out.print(CYAN + "╚═══► Enter option here: " + RESET);
+
         }
     }
 }
