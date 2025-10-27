db82d29cdf935f7844c7483c5619a4a26a70b0ac
Yuom Kitya <kityayuom@gmail.com>
2025-10-27 17:52:12 +0700

    fix bug for account.java

--- src/atm/Account.java
+++ src/atm/Account.java
@@ -1,13 +1,32 @@
 package atm;
 
 public class Account {
-    public int AccNo;
-    public int pin;
-    public double balance;
+    private final int accountNumber;
+    private String name;
+    private int pin;
+    private double balance;
 
-    public Account(int accNo, int pin, double balance) {
-        this.AccNo = accNo;
+    public Account(int accountNumber, String name, int pin) {
+        this.accountNumber = accountNumber;
+        this.name = name;
         this.pin = pin;
-        this.balance = balance;
+        this.balance = 0.0;
+    }
+
+    public int getAccountNumber() { return accountNumber; }
+    public String getName() { return name; }
+    public int getPin() { return pin; }
+    public double getBalance() { return balance; }
+
+    public void setName(String name) { this.name = name; }
+    public void setPin(int pin) { this.pin = pin; }
+    public void setBalance(double balance) { this.balance = balance; }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Account{number=%d, name='%s', balance=$%.2f}",
+                accountNumber, name, balance
+        );
     }
 }
